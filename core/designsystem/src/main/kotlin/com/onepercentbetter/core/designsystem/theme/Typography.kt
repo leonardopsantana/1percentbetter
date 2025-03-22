bf10575aa08cbe80e09452c1f01294eb48bb4e65
Leonardo Santana <leonardo.santana@cidadania4u.com.br>
2025-03-22 17:24:18 -0300

    New fonts
    Change-Id: I3904681d5ec8a45163f06cd02f69406661fdca62

--- core/designsystem/src/main/kotlin/com/onepercentbetter/core/designsystem/theme/Typography.kt
+++ core/designsystem/src/main/kotlin/com/onepercentbetter/core/designsystem/theme/Typography.kt
@@ -4,47 +4,63 @@ package com.onepercentbetter.core.designsystem.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.text.style.LineHeightStyle.Alignment
 import androidx.compose.ui.text.style.LineHeightStyle.Trim
 import androidx.compose.ui.unit.sp
+import com.onepercentbetter.core.designsystem.R
 
 /**
  * One percent better typography.
  */
+val montSerratFamily = FontFamily(
+    Font(R.font.montserrat_light, FontWeight.Light),
+    Font(R.font.montserrat_regular, FontWeight.Normal),
+    Font(R.font.montserrat_medium, FontWeight.Medium),
+    Font(R.font.montserrat_bold, FontWeight.Bold)
+)
+
 internal val OPBTypography = Typography(
     displayLarge = TextStyle(
+        fontFamily = montSerratFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 57.sp,
         lineHeight = 64.sp,
-        letterSpacing = (-0.25).sp,
+        letterSpacing = (-0.25).sp
     ),
     displayMedium = TextStyle(
+        fontFamily = montSerratFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 45.sp,
         lineHeight = 52.sp,
         letterSpacing = 0.sp,
     ),
     displaySmall = TextStyle(
+        fontFamily = montSerratFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 36.sp,
         lineHeight = 44.sp,
         letterSpacing = 0.sp,
     ),
     headlineLarge = TextStyle(
+        fontFamily = montSerratFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 32.sp,
         lineHeight = 40.sp,
         letterSpacing = 0.sp,
     ),
     headlineMedium = TextStyle(
+        fontFamily = montSerratFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 28.sp,
         lineHeight = 36.sp,
         letterSpacing = 0.sp,
     ),
     headlineSmall = TextStyle(
+        fontFamily = montSerratFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 24.sp,
         lineHeight = 32.sp,
@@ -55,6 +71,7 @@ internal val OPBTypography = Typography(
         ),
     ),
     titleLarge = TextStyle(
+        fontFamily = montSerratFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 22.sp,
         lineHeight = 28.sp,
@@ -65,12 +82,14 @@ internal val OPBTypography = Typography(
         ),
     ),
     titleMedium = TextStyle(
+        fontFamily = montSerratFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 18.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.1.sp,
     ),
     titleSmall = TextStyle(
+        fontFamily = montSerratFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 20.sp,
@@ -78,6 +97,7 @@ internal val OPBTypography = Typography(
     ),
     // Default text style
     bodyLarge = TextStyle(
+        fontFamily = montSerratFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
@@ -88,12 +108,14 @@ internal val OPBTypography = Typography(
         ),
     ),
     bodyMedium = TextStyle(
+        fontFamily = montSerratFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.25.sp,
     ),
     bodySmall = TextStyle(
+        fontFamily = montSerratFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp,
         lineHeight = 16.sp,
@@ -101,6 +123,7 @@ internal val OPBTypography = Typography(
     ),
     // Used for Button
     labelLarge = TextStyle(
+        fontFamily = montSerratFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 20.sp,
@@ -112,6 +135,7 @@ internal val OPBTypography = Typography(
     ),
     // Used for Navigation items
     labelMedium = TextStyle(
+        fontFamily = montSerratFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 12.sp,
         lineHeight = 16.sp,
@@ -123,6 +147,7 @@ internal val OPBTypography = Typography(
     ),
     // Used for Tag
     labelSmall = TextStyle(
+        fontFamily = montSerratFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 10.sp,
         lineHeight = 14.sp,
@@ -133,3 +158,4 @@ internal val OPBTypography = Typography(
         ),
     ),
 )
+
